8dc4671fd34232575c44ac9c19afceb821423135
Damien Demessence <damiend@ex-situ.com>
2015-05-22 09:46:23 -0400

    Empty Provider Added

---
+++ Providers/GeoipProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace EXS\GeoipProvider\Providers;
+
+use Pimple\ServiceProviderInterface;
+use Pimple\Container;
+
+/**
+ * Abstract services for Geoip
+ *
+ * Created 1-May-2015
+ * @author Damien Demessence <damiend@ex-situ.com>
+ * @copyright   Copyright 2015 ExSitu Marketing.
+ */
+class GeoipProvider implements ServiceProviderInterface
+{
+
+    public function register(Container $app)
+    {
+
+    }
+}
